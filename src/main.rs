7890b28af2031e960580d786cdb58e6e06a79e93
Xithrius <xithrius@gmail.com>
2022-10-02 19:13:01 -0700

    Modified to the 'nitride' project name

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 #![warn(clippy::nursery, clippy::pedantic)]
-#![allow(clippy::module_name_repetitions, clippy::future_not_send)]
 
-use color_eyre::eyre::{Error, Result, WrapErr};
-
-mod handlers;
-mod utils;
-
-use crate::handlers::config::CompleteConfig;
-
-fn main() -> Result<(), Error> {
-    color_eyre::install().unwrap();
-
-    let mut _config = CompleteConfig::new()
-        .wrap_err("Unable to read configuration file.")
-        .unwrap();
+fn main() {
+    println!("Hello, world!");
 
     std::process::exit(0)
 }
